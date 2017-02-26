f009f6ce056175cb52ac4e08ed2e7cf4e1b32564
mathieu <mathieu.besson@tuta.io>
2017-02-26 20:34:35 +0100

    Added the spark session builder

--- src/main/scala/MainClass.scala
+++ src/main/scala/MainClass.scala
@@ -1,8 +1,23 @@
+import com.mongodb.spark._
+import com.mongodb.spark.config._
+import com.mongodb.spark.rdd.MongoRDD
+import org.bson.Document
+import org.apache.spark.sql.SparkSession
+
+
 /**
   * Created by mathieu on 26/02/2017.
   */
-class MainClass {
-  def main(args: Array[String]): Unit = {
-    println ("Hello Scala By example")
-  }
+object MainClass extends App{
+  println ("Connecting Spark with MongoDB")
+
+  val spark = SparkSession.builder()
+                          .master("local")
+                          .appName("MongoSparkConnectorIntro")
+                          .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/nasa.eva")
+                          .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/nasa.astronautTotals")
+                          .getOrCreate()
+
+  spark.catalog.listDatabases()
+
 }
